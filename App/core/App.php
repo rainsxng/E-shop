25da51dd5ac340734b307b8b6a72aed115e44927
Dmitry Bohdanov <rainsxng@gmail.com / starsettime@gmail.com>
2019-01-11 15:07:56 +0200

    Jquery added

--- App/core/App.php
+++ App/core/App.php
@@ -7,12 +7,15 @@ class App
     public $route=null;
     public function  __construct()
     {
+        if (isset($_COOKIE['PHPSESSID'])){
+            session_start();
+        }
         $logger = new LoggerClass();
         $logger->setLogFile('log.log');
         $logger->registerFatalHandler();
         $logger->registerExceptionHandler();
         $logger->registerErrorHandler([],false);
-        $wrapper = new Mapper();
+        $mapper = new Mapper();
         $router = new Request();
         $router::dispatch();
     }
